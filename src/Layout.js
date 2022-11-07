f1cb78d8b98ae61c75a09936b3d8291ad55b9b80
Godswilly <kalu.agukalu@gmail.com>
2022-11-07 22:38:18 +0100

    Implement useWindowSize hook

--- src/Layout.js
+++ src/Layout.js
@@ -3,13 +3,15 @@ import Header from './Header';
 import Nav from './Nav';
 import Footer from './Footer';
 import { Outlet } from 'react-router';
+import useWindowSize from './hooks/useWindowSize';
 
 const Layout = ({ search, setSearch }) => {
+	const { width } = useWindowSize();
 	return (
 		<div className='App'>
-			<Header title='React JS Blog' />
+			<Header title='React JS Blog' width={width} />
 			<Nav search={search} setSearch={setSearch} />
-      <Outlet />
+			<Outlet />
 			<Footer />
 		</div>
 	);
